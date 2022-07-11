ec967b90d1530dba20f3b203a87845d3417943c1
Daniel <daniel_sousa.p@outlook.com>
2022-07-10 21:02:32 -0300

    feat: create getOrderById rpc

--- hermes/implementation.js
+++ hermes/implementation.js
@@ -23,4 +23,29 @@ module.exports =  {
       user_id
     });
   },
+
+  async getOrderById(req, res) {
+    const response = await Order.findById(req.request._id);
+
+    if(!response) {
+      return res(null, {
+        error: 'Order not found',
+        status: 'error'
+      });
+    }
+
+    const dishes = response.order.map(order => {
+      return {
+        dish: order.dish
+      }
+    });
+
+    return res(null, {
+      _id: response._id,
+      user_id: response.user_id,
+      order: dishes,
+      price: response.price,
+      status: 'ok'
+    });
+  }
 };
